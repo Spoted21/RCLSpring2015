502be1b4459d4fe64287becc2ac79d0c9b5885fd
Michael Floren <flor3652@bears.unco.edu>
2015-04-01 21:11:34 -0600

    Pushing a change!

--- server.R
+++ server.R
@@ -3,7 +3,16 @@
 shinyServer(function(input, output) {
   
   # You can access the value of the widget with input$action, e.g.
-  output$value <- renderPrint({ input$action })
+  output$value <- renderPrint({
+    
+    input$action
+    
+    isolate(
+      input$valuetext
+    )
+    
+  })
+  
   output$valuetext <- renderPrint({ input$valuetext })
 })
 
