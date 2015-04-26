c60f2c99dd3cd3d1fa225920298f7b0857b3a677
Michael Floren <flor3652@bears.unco.edu>
2015-04-26 11:43:36 -0600

    Conditional panel within Excel input not functioning properly

--- UI.R
+++ UI.R
@@ -1,5 +1,6 @@
-#    http://www.beardedanalytics.com/todd/iris.csv
-if(!require("readxl")) install.packages("readxl", dependencies=TRUE)
+if(!require("devtools")) install.packages("devtools", dependencies=TRUE)
+library(devtools)
+if(!require("readxl")) devtools::install_github("hadley/readxl")
 if(!require("downloader")) install.packages("downloader", dependencies=TRUE)
 if(!require("httr")) install.packages("httr", dependencies=TRUE)
 if(!require("shiny")) install.packages("shiny", dependencies=TRUE)
@@ -17,74 +18,63 @@ shinyUI(pageWithSidebar(
     tabsetPanel(
       
       tabPanel("Start",
-                
-        selectInput("locvsurl", "Select whether you'd prefer to use a local file or one from the web:",
-                    choices = list("Local File" = "local",
-                                   "URL" = "url"), selected="url"),
-        
-        textOutput("defaultText"),
-        
-        uiOutput("uiFile"),
-        # Copy the line below to make an action button
-        actionButton("action", label = "Get Data"),
-        hr(),
-        hr(),
-        h4("Data Summary"),
-        wellPanel( 
-          textOutput("DataSummaryText")
-        ),
-        hr(),
-        h4("Select Data"),
-        checkboxInput("selectData", "Check if you wish to specify columns", value=FALSE),
-        
-        hr(),
-        uiOutput("uiVar") #created on the server side to give options based on data columns
+               
+               selectInput("locvsurl", "Select whether you'd prefer to use a local file or one from the web:",
+                           choices = list("Local File" = "local",
+                                          "URL" = "url"), selected="url"),
+               
+               textOutput("defaultText"),
+               
+               uiOutput("uiFile"),
+               # Copy the line below to make an action button
+               actionButton("action", label = "Get Data"),
+               hr(),
+               h4("Data Summary"),
+               wellPanel( 
+                 textOutput("DataSummaryText")
+               ),
+               hr(),
+               h4("Select Data"),
+               checkboxInput("selectData", "Check if you wish to specify columns", value=FALSE),
+               
+               hr(),
+               uiOutput("uiVar") #created on the server side to give options based on data columns
       ),
-    
+      
       tabPanel("Change Input File Type",
-                  
-        h5("If your file isn't a CSV, please give us some information to help us read it in:"),
-        
-        #tags$sub("Note that changing the options below may remove some error checks"),
-        
-        hr(),
-        
-        checkboxInput('header', 'Check if the first row of your file contains headers', TRUE), 
-        
-        checkboxInput("xl", "Check if your file an xls or xlsx file", FALSE),
-        
-        hr(),
-        
-        conditionalPanel(
-          condition = 'input.xl==false',
-        
-          radioButtons('sep', 'Separator',
-                       c(Comma=',',
-                         Semicolon=';',
-                         Tab='\t'),
-                       ','),
-          
-          radioButtons('quote', 'Quote',
-                       c(None='',
-                         'Double Quote'='"',
-                         'Single Quote'="'"),
-                       '"'),
-          
-          radioButtons('extension', 'Is the file a csv or txt?',
-                       c(CSV='.csv',
-                         TXT='.txt'),
-                       '.csv')
-        ),
-        
-        conditionalPanel(
-          condition = 'input.xl==true',
-          
-          #this changes "extension" to be xls or xlsx, depending on the user selection (for error checking). This changes the value above, so we use '.csv' as the default (to keep .csv the default formatting overall)
-          radioButtons('extension', 'xls or xslx?',
-                       c(xls='.xls',
-                         xlsx='.xlsx'),
-                       '.csv')
-        )
+               
+               h5("If your file isn't a CSV, please give us some information to help us read it in:"),
+               
+               #tags$sub("Note that changing the options below may remove some error checks"),
+               
+               hr(),
+               
+               checkboxInput('header', 'Check if the first row of your file contains headers', TRUE), 
+               
+               checkboxInput("xl", "Check if your file an xls or xlsx file", FALSE),
+               
+               hr(),
+               
+               conditionalPanel(
+                 condition = 'input.xl==false',
+                 
+                 radioButtons('sep', 'Separator',
+                              c(Comma=',',
+                                Semicolon=';',
+                                Tab='\t'),
+                              ','),
+                 
+                 radioButtons('quote', 'Quote',
+                              c(None='',
+                                'Double Quote'='"',
+                                'Single Quote'="'"),
+                              '"'),
+                 
+                 radioButtons('extension', 'Is the file a csv or txt?',
+                              c(CSV='.csv',
+                                TXT='.txt'),
+                              '.csv')
+               )
       )
     )
   ),
